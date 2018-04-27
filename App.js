66680ff62dc45b1dd65806057ee494d7817ff726
amatiyko <andrew.matiyko@gmail.com>
2018-04-28 01:53:12 +0300

    add redux. create store

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './src/components';
+import { Provider } from 'react-redux'
 
-export default class App extends React.Component {
+import { Store } from './src/Store';
+import { Header, CryptoContainer } from './src/components';
+
+console.log('================');
+console.log('store', Store);
+console.log('================');
+
+export default class App extends Component {
   render() {
     return (
-      <View>
-        <Header/>
-      </View>
+     <Provider store={Store}>
+        <View>
+          <Header />
+          <CryptoContainer />
+        </View>
+      </Provider>
     );
   }
 }
