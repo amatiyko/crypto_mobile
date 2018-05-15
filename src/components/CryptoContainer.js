de9544624672cc304a0af7d4ca4bd13a68ec51aa
amatiyko <andrew.matiyko@gmail.com>
2018-05-16 00:11:29 +0300

    coin item

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import FetchCoinData from "./../Actions/FetchCoinData";
+import CoinItem from "./CoinItem";
+
+const styles = StyleSheet.create({
+    contentContainer: {
+        paddingBottom: 100,
+    }
+});
 
 class CryptoContainer extends Component {
     componentDidMount() {
@@ -9,11 +16,27 @@ class CryptoContainer extends Component {
     }
     
     render() {
-        return (
-            <View>
-                <Text>Container</Text>
-            </View>
-        )
+        if (this.props.crypto.data) {
+            let data = this.props.crypto.data;
+            return (
+                <ScrollView contentContainerStyle={styles.contentContainer}>
+                    {data.map((item, index) => {
+                            return  (
+                                <CoinItem 
+                                    key={index}
+                                    data={item}
+                               />
+                            )
+                        }
+                    )}
+                </ScrollView>
+            )
+        } else {
+            return (
+                <View>Loading...</View>
+            )
+        }
+        
     }
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Header from "./Header.js";
 import CryptoContainer from "./CryptoContainer";
+import CoinItem from "./CoinItem";
 
-export { Header, CryptoContainer };
+export { Header, CryptoContainer, CoinItem };
